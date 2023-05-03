f428f63b5896bb6a6505abd9a58990ed46d7f08a
Andreatl1 <80221040+Andreatl1@users.noreply.github.com>
2023-05-03 23:18:48 +0200

    Update auto_renew_posts.py

--- auto_renew_posts.py
+++ auto_renew_posts.py
@@ -11,7 +11,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 class App:
-    def __init__(self, email= "", password= "", language="", main_url="", marketplace_your_posts="", binary_location="", driver_location="", time_to_sleep=""):
+    def __init__(self, email= "", password= "", language="", main_url="", marketplace_your_posts="", marketplace_relist_posts="", binary_location="", driver_location="", time_to_sleep=""):
         self.email = email
         self.password = password
         self.language = language
@@ -30,11 +30,13 @@ class App:
         self.driver.maximize_window()
         self.main_url = main_url
         self.marketplace_your_posts = marketplace_your_posts
+        self.marketplace_relist_posts = marketplace_relist_posts
         self.driver.get(self.main_url)
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@data-cookiebanner="accept_button"]'))).click() #per rimuover il pop-up dei cookies
         self.log_in()
         self.move_from_home_to_marketplace_renew_posts()
-        self.renew_all_post() 
+        self.renew_all_post()
+        self.relist_all_post()
         sleep(self.time_to_sleep)
         self.driver.quit()
         
@@ -57,12 +59,19 @@ class App:
         sleep(6)
         renew_buttons = self.driver.find_elements(By.XPATH, "//div[@aria-label='Rinnova']")
         
-        if renew_buttons: #check if empty list
+        if renew_buttons: #check if the buttons exists
             for renew_button in renew_buttons:
                 renew_button.click()
-                sleep(self.time_to_sleep)
+                sleep(self.time_to_sleep * 2)
 
-
+    def relist_all_post(self):
+        sleep(6)
+        relist_buttons = self.driver.find_elements(By.XPATH, "//div[@aria-label='Elimina e Metti di nuovo in vendita']")
+        
+        if relist_buttons: #check if the buttons exists
+            for relist_button in relist_buttons:
+                relist_button.click()
+                sleep(self.time_to_sleep * 2)
 
 
 if __name__ == '__main__':
@@ -70,4 +79,4 @@ if __name__ == '__main__':
     config_object.read("config.ini")
     facebook = config_object["FACEBOOK"]
     configuration = config_object["CONFIG"]
-    app = App(facebook["email"], facebook["password"], configuration["language"], facebook["main_url"], facebook["marketplace_renew_posts"], configuration["binary_location"], configuration["driver_location"], configuration["time_to_sleep"])
+    app = App(facebook["email"], facebook["password"], configuration["language"], facebook["main_url"], facebook["marketplace_renew_posts"], facebook["marketplace_relist_posts"], configuration["binary_location"], configuration["driver_location"], configuration["time_to_sleep"])
